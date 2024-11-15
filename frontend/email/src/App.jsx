a5cc710bf06570a18d9cb9082ec79cf8dde94cc1
Naveen_Kumar <126059014+n-a-v-e-e@users.noreply.github.com>
2024-11-15 20:02:10 +0530

    Update App.jsx

--- frontend/email/src/App.jsx
+++ frontend/email/src/App.jsx
@@ -12,7 +12,7 @@ export default function App() {
 
   const handleCheck = async () => {
     try {
-      const response = await fetch('http://127.0.0.1:5000/predict', {
+      const response = await fetch('https://email-api-zbes.onrender.com/predict', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
